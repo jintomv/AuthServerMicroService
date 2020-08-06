2c43adfa5935a740897e2402a8f01559724abac3
KRISHNACHANDRAN\krishnachandran <eskc1981@gmail.com>
2020-08-06 12:35:45 +0530

    Main class

--- src/main/java/com/example/AuthorizationServer/AuthorizationServerApplication.java
+++ src/main/java/com/example/AuthorizationServer/AuthorizationServerApplication.java
@@ -3,6 +3,7 @@ package com.example.AuthorizationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is the main class
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
