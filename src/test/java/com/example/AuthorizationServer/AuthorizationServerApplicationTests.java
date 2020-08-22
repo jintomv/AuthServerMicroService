7ca16bd0cff42916a109d74f8843f4ed34040f94
KRISHNACHANDRAN\krishnachandran <eskc1981@gmail.com>
2020-08-22 20:03:50 +0530

    halo

--- src/test/java/com/example/AuthorizationServer/AuthorizationServerApplicationTests.java
+++ src/test/java/com/example/AuthorizationServer/AuthorizationServerApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.AuthorizationServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 //testing Merging 123467889
+//Halo Merge testing
 @SpringBootTest
 class AuthorizationServerApplicationTests {
 
